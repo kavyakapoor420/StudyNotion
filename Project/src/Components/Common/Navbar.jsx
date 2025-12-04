6806d63c580a2e2dd30a1ed97933809d7037e887
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-12-04 21:54:13 +0530

    stunded enrolled into course after payment capture and veirfy payment , email also sent

--- Project/src/Components/Common/Navbar.jsx
+++ Project/src/Components/Common/Navbar.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, matchPath } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import Logo from '../../assets/Logo/Logo-Full-Light.png'
 import { NavbarLinks } from '../../data/navbar-links'
 import { useSelector } from 'react-redux'
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+
 import ProfileDropdown from '../ProfileDropdown'
+
+import { categories } from '../../Services/api'
+import { apiConnector } from '../../Services/apiConnector'
+
+const subLinks=[
+    {
+      title:'python',
+      link:"/catalog/python"
+    },{
+      title:'web dev',
+      link:"/catalog/web-development"
+    }
+]
 
 const Navbar = () => {
 
@@ -13,6 +28,21 @@ const Navbar = () => {
     const {user}=useSelector((state)=>state.profile)
     const {totalItems}=useSelector((state)=>state.cart)
 
+    // const [subLinks,setSubLinks]=useState([])
+
+    // const fetchSubLinks=async()=>{
+    //        try{
+    //             const result=await apiConnector("GET",categories.CATEGORIES_API)
+    //             console.log('printing sublinks results',result)
+    //             setSubLinks(result.data.data)
+    //        }catch(err){
+    //              console.log('could not fetch the category list ')
+    //        }
+    //     }
+
+    // useEffect(()=>{
+    //       fetchSubLinks() 
+    // },[])
 
     const location=useLocation() 
    const matchRoute=(route)=>{
@@ -36,7 +66,29 @@ const Navbar = () => {
                                          <li key={indx}>  
                                               {
                                                 link.title==='Catalog' ? (
-                                                <div></div>
+                                                <div className='relative  flex items-center gap-2 group'>
+                                                      <p>{link?.title}</p>
+                                                      <IoIosArrowDropdownCircle />
+                                                        
+                                                      <div className='invisible absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[45%] flex flex-col rounded-md text-richblack-900 bg-richblack-5 p-4 transition-all duration-200 group-hover:visible group-hover:opacity-100 lg:w-[300px]'>
+                                                                <div className="absolute left-[50%] top-0 rotate-45 rounded bg-richblack-5 h-6 w-6 translate-y-[80%] translate-x-[-50%] ">
+                                                                          {
+                                                                            subLinks.length ? (
+                                                                                    
+                                                                                      subLinks.map((subLink,indx)=>(
+                                                                                          <Link key={indx} to={ `${subLink.link}`}>
+                                                                                                <p>{subLink.link}</p>
+                                                                                          </Link>
+                                                                                      ))
+                                                                                  
+                                                                            )        : (
+                                                                              <div></div>
+                                                                            )
+                                                                          }
+                                                                </div>
+                                                      </div>
+
+                                                </div>
                                               ) :(
                                                    <Link to={link?.path}>
                                                         <p className={`${matchRoute(link?.path) ? "text-yellow-50" : "text-richblack-50"}`}>
--- backend/Controllers/PaymentController.js
+++ backend/Controllers/PaymentController.js
@@ -5,6 +5,116 @@ const Course = require('../Models/Course')
 const User = require('../Models/User')
 const mailSender = require('../utils/mailSender')
 
+// without using webhook u can read razorpay docs 
+// func for order initiate write another fnc to verify Payment
+exports.capturePayment2=async(req,res)=>{
+     
+     const {courses}=req.body 
+     const userId=req.user.id 
+
+     if(courses.length===0){
+         return res.json({success:false,message:'please provide course ID '})
+     }
+
+     let totalAmout=0;
+
+     for(const course_id of courses){
+        let course;
+
+        try{
+             course=await Course.findById(course_id)
+             if(!course){
+                return res.status(200).json({success:false,message:'could not find the course'})
+             }
+
+             const uid=new mongoose.Types.ObjectId(userId)
+
+             if(course.studentsEnrolled.includes(uid)){
+                return res.status(200).json({success:false,message:'student is already enrolled'})
+             }
+
+             totalAmout+=course.price
+
+        }catch(err){
+            console.log(err)
+            return res.status(500).json({success:false,message:err.message})
+        }
+     }
+
+     const options={
+        amout:totalAmout*100,
+        currency:"INR",
+        receipt:Math.random(Date.now()).toString() 
+     }
+
+     try{
+          const paymentResponse=await instance.create(options)
+          res.json({
+            success:true,data:paymentResponse
+          })
+     }catch(err){
+          console.log(err)
+          return res.status(500).json({success:false,message:'could not intitate order of payment '})
+     }
+}
+
+exports.verifySignature2=async(req,res)=>{
+
+     const userId=req.user.id 
+     const razorpay_order_id=req.body?.razorpay_order_id
+     const courses=req.body?.courses
+     const razorpay_signature=req.body?.signature
+     const razorpay_payment_id=req.body?.razorpay_payment_id
+     
+     if(!razorpay_order_id || !razorpay_payment_id || !razorpay_signature || !courses || !userId){
+         return res.status(200).json({success:false,message:'payment failed'})
+     }
+
+
+     let body=razorpay_order_id + "|" + razorpay_payment_id
+     const expectedSignature=crypto
+                            .createHmac("sha256",process.env.RAZORPAY_WEBHOOK_SECRET)
+                            .update(body.toString())
+                            .digest("hex")
+
+    if(expectedSignature===razorpay_signature){
+        // enroll  kro stundent to course 
+
+        await enrollStudents()
+        return res.status(200).json({success:true,message:'payment verified'})
+    }
+
+    return res.status(200).json({success:false,message:'payment failed'})
+
+}
+
+const enrollStudents=async(courses,userId,res)=>{
+      
+    if(!courses || !userId){
+        return res.status(400).json({success:false,message:'please provide data for courses or userid'})
+    }
+
+    for (const course_id of courses){
+        // find the course and enroll the student in it
+        const enrolledCourse=await Course.findOneAndUpdate(
+            {_id:course_id},
+            {$push:{studentsEnrolled:userId}},
+            {new:true}
+        )
+
+        if(!enrolledCourse){
+            return res.status(500).json({success:false,message:'course not found'})
+        }
+
+        // find the student and add the course to their list of enrolledCourses 
+        const enrolledStudent=await User.findByIdAndUpdate(userId,{$push:{courseId:course_id}},{new:true})
+
+        const emailResponse=await mailSender(
+            enrollStudents.email,
+            ``
+        )
+    }
+}
 
 // capture payment and create order -> payment gateway -> razorpay
 
