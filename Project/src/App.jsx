cdf4e3dd0592534fc1eaf8ed5fbb693166be6c75
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-12-04 19:43:10 +0530

    apiConnector for api call written

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -14,8 +14,6 @@ const App = () => {
               <Route path='/' element={<HomePage/>}/>
           </Routes>
        <Footer/>
-       
-       <Recipes/>
 
     </div>
   )
--- Project/src/Components/Common/Navbar.jsx
+++ Project/src/Components/Common/Navbar.jsx
@@ -3,8 +3,16 @@ import { Link, matchPath } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import Logo from '../../assets/Logo/Logo-Full-Light.png'
 import { NavbarLinks } from '../../data/navbar-links'
+import { useSelector } from 'react-redux'
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import ProfileDropdown from '../ProfileDropdown'
 
 const Navbar = () => {
+
+    const {token}=useSelector((state)=>state.auth)
+    const {user}=useSelector((state)=>state.profile)
+    const {totalItems}=useSelector((state)=>state.cart)
+
 
     const location=useLocation() 
    const matchRoute=(route)=>{
@@ -27,7 +35,9 @@ const Navbar = () => {
                                     NavbarLinks.map((link,indx)=>(
                                          <li key={indx}>  
                                               {
-                                                link.title==='Catalog' ? (<div></div>) :(
+                                                link.title==='Catalog' ? (
+                                                <div></div>
+                                              ) :(
                                                    <Link to={link?.path}>
                                                         <p className={`${matchRoute(link?.path) ? "text-yellow-50" : "text-richblack-50"}`}>
                                                                 {link.title}
@@ -44,7 +54,45 @@ const Navbar = () => {
 
     {/* token===null not logged in else signup btn but it token!=null profile dropdown menu && user && user!=Instructor */}
                   {/* login signup btns  */}
-                  <div className='flex gap-4 '>
+                  <div className='flex gap-x-4 items-center '>
+                          
+                          {
+                             user && user?.accountType!=="Instructor" && (
+                                <Link to='/dashboard/cart' className='relative'>
+                                     <AiOutlineShoppingCart/>
+                                     {
+                                        totalItems>0 && (
+                                          <span>
+                                              {totalItems}
+                                          </span>
+                                        )
+                                     }
+                                </Link>
+                             )
+                          }
+
+                          {
+                             token==null && (
+                                <Link to={'/login'}>
+                                    <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[10px] rounded-md text-richblack-100'>
+                                       LOGIN 
+                                    </button>
+                                </Link>
+                             )
+                          }
+                          {
+                            token===null && (
+                              <Link to={'/signup'}>
+                                     <button className='border border-richblack-700 bg-richblack-800 px-[12px] py-[10px] rounded-md text-richblack-100'>
+                                      SIGNUP 
+                                     </button>
+                              </Link>
+                            )
+                          }
+
+                          {
+                            token!==null  && <ProfileDropdown/>
+                          }
 
                   </div>
           </div>
