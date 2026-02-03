f92e135a8d0cf834a5708237188590775c51209f
kavyakapoor420 <kavyakapoor412@gmail.com>
2026-02-04 00:01:16 +0530

    written mail sender utility method

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -4,9 +4,26 @@ const nodemailer=require('nodemailer')
 const mailSender=async(email ,title,body)=>{
 
     try{
+        // create transporter
+        const transporter=nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user:process.env.MAIL_USER ,
+                pass:process.env.MAIL_PASS 
+            }
+        })
 
+        let info=await transporter.sendMail({
+            from : "StudyNotion || created by your own KAvya Kapoor",
+            to:`${email}`,
+            subject:`${title}`,
+            html:`<div>${body}</div>`
+        })
+        console.log("mail info",info)
+        return info 
     }catch(err){
-
+         console.log("error in mail sender util",err)
     }
-    
+
 }
+module.exports=mailSender ;
